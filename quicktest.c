556f9457ad2e75f208935f26dd3c2344f8c15a2a
sansna <1185280650@qq.com>
2017-02-26 19:33:17 +0800

    added float types to quicktest

--- quicktest.c
+++ quicktest.c
@@ -7,7 +7,10 @@ int main() {
   printf( "     long int: %d\n", sizeof(long int) ) ;
   printf( "long long int: %d\n", sizeof(long long int) ) ;
   printf( "       size_t: %d\n", sizeof(size_t) ) ;
-  printf( "        void*: %d\n\n", sizeof(void *) ) ;
+  printf( "        void*: %d\n", sizeof(void *) ) ;
+  printf( "        float: %d\n", sizeof(float) ) ;
+  printf( "       double: %d\n", sizeof(double) ) ;
+  printf( "  long double: %d\n\n", sizeof(long double) ) ;
 
 
   printf( "PRIu32 usage (see source): %"PRIu32"\n" , (uint32_t) 42 ) ;

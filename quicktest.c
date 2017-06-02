896f887f8e9d065c8f38557fa2ea2040214bdb9c
sansna <1185280650@qq.com>
2017-06-02 02:49:00 +0000

    added malloc alignment test to quicktest

--- quicktest.c
+++ quicktest.c
@@ -1,19 +1,31 @@
 #include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  printf( "    short int: %d\n" , sizeof(short int) ) ;
-  printf( "          int: %d\n" , sizeof(int) ) ;
-  printf( "     long int: %d\n", sizeof(long int) ) ;
-  printf( "long long int: %d\n", sizeof(long long int) ) ;
-  printf( "       size_t: %d\n", sizeof(size_t) ) ;
-  printf( "        void*: %d\n", sizeof(void *) ) ;
-  printf( "        float: %d\n", sizeof(float) ) ;
-  printf( "       double: %d\n", sizeof(double) ) ;
-  printf( "  long double: %d\n\n", sizeof(long double) ) ;
+    char *a = (char *)malloc(1);
+    char *b = (char *)malloc(1);
+    char *c = (char *)malloc(b-a);
+    char *d = (char *)malloc(1);
+    char *e = (char *)malloc(d-c);
+    char *f = (char *)malloc(1);
+    printf( "    short int: %d\n" , sizeof(short int) ) ;
+    printf( "          int: %d\n" , sizeof(int) ) ;
+    printf( "     long int: %d\n", sizeof(long int) ) ;
+    printf( "long long int: %d\n", sizeof(long long int) ) ;
+    printf( "       size_t: %d\n", sizeof(size_t) ) ;
+    printf( "        void*: %d\n", sizeof(void *) ) ;
+    printf( "        float: %d\n", sizeof(float) ) ;
+    printf( "       double: %d\n", sizeof(double) ) ;
+    printf( "  long double: %d\n\n", sizeof(long double) ) ;
 
+    fprintf(stdout, "System malloc uses %d..%d..%d.. alignment.\n",
+            b - a,
+            d - c,
+            f - e);
 
-  printf( "PRIu32 usage (see source): %"PRIu32"\n\n" , (uint32_t) 42 ) ;
-  printf( "__WORDSIZE(fastest size)= %d\n", __WORDSIZE);
-  return 0;
+    printf( "PRIu32 usage (see source): %"PRIu32"\n\n" , (uint32_t) 42 ) ;
+    printf( "__WORDSIZE(fastest size)= %d\n", __WORDSIZE);
+
+    return 0;
 }

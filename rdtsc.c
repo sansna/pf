c37d0911310df7d9aaa2b064f37a946137634623
sansna <1185280650@qq.com>
2017-08-25 16:52:13 +0800

    rdtsc measurement improve

--- rdtsc.c
+++ rdtsc.c
@@ -2,9 +2,6 @@
 #include<stdlib.h>
 #include<linux/types.h>
 
-#define TIMES 0
-#define SIZE 1024
-
 __u64 rdtsc()
 {
     __u32 lo,hi;
@@ -16,33 +13,30 @@ __u64 rdtsc()
     return (__u64)hi<<32|lo;
 }
 
-int myfunction()
+int myfunction(int i)
 {
-    int i;
-    char *p = NULL;
-    for(i = 0;i<TIMES;i++)
-    {
-        p = (char*)malloc(SIZE*sizeof(char));
-        if(p)
-        {
-            free(p);
-        }
-        else
-        {
-            printf("malloc failed when i = %d\n",i);
-        }
-    }
-    return 0;
+	i *= i;
+    return i;
 }
+
 int test_rdtsc()
 {
     __u64 begin;
     __u64 end;
+	__u64 i = 0;
+	__u64 TRIES = 3000000000; /* 3G, result convergence */
+	int j = 0;
 
     begin = rdtsc();
-    myfunction();
-    end = rdtsc();
-    printf("myfunction cost %llu CPU cycles\n",end-begin);
+	/* Circumvent OOO */
+	if (begin) {
+		for (; i < TRIES; i ++)
+			j += myfunction(i);
+	}
+	/* Circumvent OOO */
+	if (j) /* Unlikely to be 0 */
+		end = rdtsc();
+    printf("myfunction cost %llu/10000 CPU cycles\n",(end-begin)*10000/TRIES);
     return 0;
 }
 

4e1312832efe9745f8ff768744f903d7b7a422e7
sansna <1185280650@qq.com>
2017-12-21 14:42:47 +0800

    intro updated

--- stack-abuse/stack-above-main.c
+++ stack-abuse/stack-above-main.c
@@ -1,3 +1,15 @@
+/* stack-above-main.c: In this snippet, we change rbp or rsp in function().
+ *
+ * - When we change rbp, the back trace printed by gdb will be changed, because
+ * gdb uses rbp to determing stack. but the whole flow of the function will not
+ * be changed, because that is determined by rsp.
+ *
+ * - When we change rsp, the back trace printed by gdb in function will *not* be
+ * changed because gdb uses rbp to determine back trace. But when we returned
+ * out of function, we go to the stack which is above main, and thus its rbp is
+ * changed to the one above main. Thus the print result will be the stack above
+ * main.
+ */
 #include <stdio.h>
 
 int function(int a, int b, int c, int d)
